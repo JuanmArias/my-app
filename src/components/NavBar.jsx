c5dc5caf559e8931df566bf7a9a857fd7ad1f3f2
JuanmArias <juanma-adtr@hotmail.com>
2021-10-21 20:49:13 -0300

    salio

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,27 @@
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container'
+import { Row } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
 
 const NavBar = () => {
      return <>
              <Navbar bg="dark">
   <Container>
     <Navbar.Brand href="#home">
-      <img
-        src=".../logo1.jpg"
-        width="30"
-        height="30"
-        className="d-inline-block align-top"
-        alt="Mi Dulce Locura"
-      />
+      <Row className="align-items-center">
+        <Col md={5}>
+          <img
+            src="../logo192.png"
+            width="100"
+            height="100"
+            className="d-inline-block align-top"
+            alt="Mi Dulce Locura"
+          />
+        </Col>
+        <Col md={7}>
+          <h3>Mi Dulce Locura</h3>
+        </Col>
+      </Row>
     </Navbar.Brand>
   </Container>
   </Navbar>
